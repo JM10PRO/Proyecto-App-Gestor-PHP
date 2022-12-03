7fe739c1cc8820ba0225f9479f33603f38af67ab
José María <josemariagl99@gmail.com>
2022-12-03 14:01:36 +0100

    first commit

---
+++ view/_template.blade.php
@@ -0,0 +1,43 @@
+<!-- 
+LAYOUT DE LA APLICACIÓN 
+ESTA PÁGINA DISPONE DONDE IRÁN LOS DIFERENTES BLOQUES QUE CONFORMAN LA APLICACIÓN
+
+Se centra solamente en la presentación.
+Deberemos indicarle:
+    - titulo
+    - menu
+    - cuerpo
+
+Podría tener tantos parámetros como quisiesemos
+Igualmente nuestra aplicación podría tener tantos layouts como deseasemos
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Gestor Tareas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div id="encabezado">
+            Bunglebuild S.L
+        </div>
+        <nav id="menu" class="navbar bg-light">
+            @include('menu')
+        </nav>
+    </header>
+    <main id="cuerpo">
+        @yield('cuerpo')
+    </main>
+    <footer id="pie">
+        &copy; Copyright. Todos los derechos reservados. <br> Autor: José María Gil Leal
+    </footer>
+</body>
+
+</html>
---
+++ view/menu.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+    <a href="{{ BASE_URL }}" class="btn btn-light">Inicio</a>
+    <a href="<?= BASE_URL ?>pag1" class="btn btn-light">Pag. 1</a>
+    <a href="<?= BASE_URL ?>listar" class="btn btn-light">Listar</a>
+    <a href="<?= BASE_URL ?>add" class="btn btn-light">Alta</a>
+    <a href="<?= BASE_URL ?>logout" class="btn btn-light">Logout</a>
+</div>
