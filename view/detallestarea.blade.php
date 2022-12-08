f16883750179ea8a787339af75292206958802e2
José María <josemariagl99@gmail.com>
2022-12-08 12:27:10 +0100

    tabla responsive

--- view/detallestarea.blade.php
+++ view/detallestarea.blade.php
@@ -10,57 +10,59 @@
 
 @section('cuerpo')
 <h1>Detalles de la tarea</h1>
-<table class="table table-striped table-hover">
-    <thead>
-        <tr>
-            <td>Id</td>
-            <td>NIF</td>
-            <td>Persona de contacto</td>
-            <td>Teléfono</td>
-            <td>Correo</td>
-            <td>Población</td>
-            <td>Código postal</td>
-            <td>Provincia</td>
-            <td>Dirección</td>
-            <td>Estado</td>
-            <td>Fecha de creación</td>
-            <td>Fecha de operario</td>
-            <td>Fecha de realización</td>
-            <td>Anotación anterior</td>
-            <td>Anotación posterior</td>
-            <td>Descripción</td>
-            <td>Fichero resumen</td>
-            <td>Fotos</td>
-            <td>Opciones</td>
-        </tr>
-    </thead>
-    <tbody class="table-group-divider">
-        <tr>
-            <td>{{$tarea['id']}}</td>
-            <td>{{$tarea['nif']}}</td>
-            <td>{{$tarea['personacontacto']}}</td>
-            <td>{{$tarea['telefono']}}</td>
-            <td>{{$tarea['correo']}}</td>
-            <td>{{$tarea['poblacion']}}</td>
-            <td>{{$tarea['codpostal']}}</td>
-            <td>{{$tarea['provincia']}}</td>
-            <td>{{$tarea['direccion']}}</td>
-            <td>{{$tarea['estado']}}</td>
-            <td>{{$tarea['fechacreacion']}}</td>
-            <td>{{$tarea['operario']}}</td>
-            <td>{{$tarea['fecharealizacion']}}</td>
-            <td>{{$tarea['anotacionanterior']}}</td>
-            <td>{{$tarea['anotacionposterior']}}</td>
-            <td>{{$tarea['descripcion']}}</td>
-            <td>{{$tarea['ficheroresumen']}}</td>
-            <td>{{$tarea['fotos']}}</td>
-            <td>
-                <a class="btn btn-primary" href="<?= BASE_URL ?>edit?id={{$tarea['id']}}">Modificar</a>
-                <a class="btn btn-danger" href="<?= BASE_URL ?>confirmardelete?id={{$tarea['id']}}">Borrar</a>
-            </td>
-        </tr>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>NIF</td>
+                <td>Persona de contacto</td>
+                <td>Teléfono</td>
+                <td>Correo</td>
+                <td>Población</td>
+                <td>Código postal</td>
+                <td>Provincia</td>
+                <td>Dirección</td>
+                <td>Estado</td>
+                <td>Fecha de creación</td>
+                <td>Fecha de operario</td>
+                <td>Fecha de realización</td>
+                <td>Anotación anterior</td>
+                <td>Anotación posterior</td>
+                <td>Descripción</td>
+                <td>Fichero resumen</td>
+                <td>Fotos</td>
+                <td>Opciones</td>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <tr>
+                <td>{{$tarea['id']}}</td>
+                <td>{{$tarea['nif']}}</td>
+                <td>{{$tarea['personacontacto']}}</td>
+                <td>{{$tarea['telefono']}}</td>
+                <td>{{$tarea['correo']}}</td>
+                <td>{{$tarea['poblacion']}}</td>
+                <td>{{$tarea['codpostal']}}</td>
+                <td>{{$tarea['provincia']}}</td>
+                <td>{{$tarea['direccion']}}</td>
+                <td>{{$tarea['estado']}}</td>
+                <td>{{$tarea['fechacreacion']}}</td>
+                <td>{{$tarea['operario']}}</td>
+                <td>{{$tarea['fecharealizacion']}}</td>
+                <td>{{$tarea['anotacionanterior']}}</td>
+                <td>{{$tarea['anotacionposterior']}}</td>
+                <td>{{$tarea['descripcion']}}</td>
+                <td>{{$tarea['ficheroresumen']}}</td>
+                <td>{{$tarea['fotos']}}</td>
+                <td>
+                    <a class="btn btn-primary" href="<?= BASE_URL ?>edit?id={{$tarea['id']}}">Modificar</a>
+                    <a class="btn btn-danger" href="<?= BASE_URL ?>confirmardelete?id={{$tarea['id']}}">Borrar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 <br>
 <a class="btn btn-secondary" href="<?= BASE_URL ?>listar">Volver al listado</a>
 @endsection
--- view/listar.blade.php
+++ view/listar.blade.php
@@ -10,70 +10,72 @@
 
 @section('cuerpo')
 <h1>{{$operacion}}</h1>
-<table class="table table-striped table-hover">
-    <thead>
-        <tr>
-            <td>Id</td>
-            <td>NIF</td>
-            <td>Persona de contacto</td>
-            <td>Estado de la tarea</td>
-            <td>Operario</td>
-            <td>Fecha de realización</td>
-            <td>Descripción</td>
-            <td>Opciones</td>
-        </tr>
-    </thead>
-    <tbody class="table-group-divider">
-        @foreach($tareas as $tarea)
-        <tr>
-            <td>{{$tarea['id']}}</td>
-            <td>{{$tarea['nif']}}</td>
-            <td>{{$tarea['personacontacto']}}</td>
-            <td>{{$tarea['estado']}}</td>
-            <td>{{$tarea['operario']}}</td>
-            <td>{{$tarea['fecharealizacion']}}</td>
-            <td>{{$tarea['descripcion']}}</td>
-            <td>
-                <a class="btn btn-secondary" href="<?= BASE_URL ?>detalles?id={{$tarea['id']}}">Detalles</a>
-                <a class="btn btn-primary" href="<?= BASE_URL ?>edit?id={{$tarea['id']}}">Modificar</a>
-                <a class="btn btn-danger" href="<?= BASE_URL ?>confirmardelete?id={{$tarea['id']}}">Borrar</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
+<div class="table-responsive">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>NIF</td>
+                <td>Persona de contacto</td>
+                <td>Estado de la tarea</td>
+                <td>Operario</td>
+                <td>Fecha de realización</td>
+                <td>Descripción</td>
+                <td>Opciones</td>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @foreach($tareas as $tarea)
+            <tr>
+                <td>{{$tarea['id']}}</td>
+                <td>{{$tarea['nif']}}</td>
+                <td>{{$tarea['personacontacto']}}</td>
+                <td>{{$tarea['estado']}}</td>
+                <td>{{$tarea['operario']}}</td>
+                <td>{{$tarea['fecharealizacion']}}</td>
+                <td>{{$tarea['descripcion']}}</td>
+                <td>
+                    <a class="btn btn-secondary" href="<?= BASE_URL ?>detalles?id={{$tarea['id']}}">Detalles</a>
+                    <a class="btn btn-primary" href="<?= BASE_URL ?>edit?id={{$tarea['id']}}">Modificar</a>
+                    <a class="btn btn-danger" href="<?= BASE_URL ?>confirmardelete?id={{$tarea['id']}}">Borrar</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-left">
         <!-- Ir a la primera página -->
-            <li class="page-item">
-                <a class="page-link" href="?pagina=1"> Primera página </a>
-            </li>
+        <li class="page-item">
+            <a class="page-link" href="?pagina=1"> Primera página </a>
+        </li>
         <!-- Ir a la página anterior -->
         @if($pagactual == 1)
-            <li class="page-item disabled">
-                <a class="page-link" href="?pagina={{$pagactual-1}}"> << </a>
-            </li>
+        <li class="page-item disabled">
+            <a class="page-link" href="?pagina={{$pagactual-1}}">
+                << </a>
+        </li>
         @else
-            <li class="page-item">
-                <a class="page-link" href="?pagina={{$pagactual-1}}"> << </a>
-            </li>        
+        <li class="page-item">
+            <a class="page-link" href="?pagina={{$pagactual-1}}">
+                << </a>
+        </li>
         @endif
         <!-- Páginas disponibles -->
-        @for ($i = 1; $i <= $totalpags; $i++) 
-            <li class="page-item"><a class="page-link" href="?pagina={{$i}}">{{$i}}</a></li>
-        @endfor
-        <!-- Ir a la página siguiente -->
-        @if($pagactual == $totalpags)
+        @for ($i = 1; $i <= $totalpags; $i++) <li class="page-item"><a class="page-link" href="?pagina={{$i}}">{{$i}}</a></li>
+            @endfor
+            <!-- Ir a la página siguiente -->
+            @if($pagactual == $totalpags)
             <li class="page-item disabled">
                 <a class="page-link" href="?pagina={{$pagactual+1}}"> >> </a>
             </li>
-        @else
+            @else
             <li class="page-item">
                 <a class="page-link" href="?pagina={{$pagactual+1}}"> >> </a>
-            </li>      
-        @endif
-        <!-- Ir a la última página -->
+            </li>
+            @endif
+            <!-- Ir a la última página -->
             <li class="page-item">
                 <a class="page-link" href="?pagina={{$totalpags}}"> Última página </a>
             </li>
